373e2e394e51c58aa7630790e53f3f2a20fce085
John Watterson <john@breakthrewbrew.com>
2020-05-06 16:16:08 -0600

    :tada: :crystal_ball: Initial commit - set up App to render menu news and search sections, set up state to hold all articles, add method that changes the currently viewed news

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
+import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm';
+import NewsContainer from '../NewsContainer/NewsContainer';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local: local,
+      health: health,
+      science: science,
+      technology: technology,
+      entertainment: entertainment,
+      currentlyDisplayed: local,
     }
   }
 
+  displaySelectedNews = (newsType) => {
+    this.setState({currentlyDisplayed: this.state[newsType]})
+  };
+
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
+      <main className="app">
+        <Menu displaySelectedNews={this.displaySelectedNews}/>
+        <SearchForm />
+        <NewsContainer currentlyDisplayed={this.state.currentlyDisplayed}/>
+      </main>
     );
   }
 }
