174e4f0f9795a258317d0dc578ca3670c6151521
John Watterson <john@breakthrewbrew.com>
2020-05-06 16:27:38 -0600

    :tada: :crystal_ball: Initial commit - Adds JSX that renders search bar and button from within a class component for SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <section className="search-section">
+        <input placeholder="Search All Article Here"></input>
+        <button>Search</button>
+      </section>
+    );
+  }
+}
 
 export default SearchForm;
