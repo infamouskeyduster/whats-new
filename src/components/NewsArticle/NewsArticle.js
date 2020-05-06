0b729ec30746d3f5507875c1d7f46ad42daba5a3
John Watterson <john@breakthrewbrew.com>
2020-05-06 16:21:25 -0600

    :tada: :crystal_ball: Adds JSX that builds out each individual news article child component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+  return(
+    <article className="news-article">
+      <h3>{props.headline}</h3>
+      <p>{props.description}</p>
+      <img src={props.img} alt=""/>
+      <a href={props.url}>Click Here to See Full Article</a>
+    </article>
+  );
+}
 
 export default NewsArticle;
