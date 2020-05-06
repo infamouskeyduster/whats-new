ccd5222fe1234f24bc9cbc73cb8c9e1cbc72be34
John Watterson <john@breakthrewbrew.com>
2020-05-06 16:24:50 -0600

    :tada: :crystal_ball: Initial commit - Adds JSX that renders a single container that holds all child news articles that are currently in state prop currentlyDisplayed

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = (props) => {
+  const currentArticles = props.currentlyDisplayed.map(currentArticle => {
+    return(
+      <NewsArticle
+      headline={currentArticle.headline}
+      description={currentArticle.description}
+      img={currentArticle.img}
+      url={currentArticle.url}
+      />
+    );
+  });
+  return(
+    <aside className="news-articles-container">
+      {currentArticles}
+    </aside>
+  );
+}
 
 export default NewsContainer;
