c26244cc84b043dc7261de07872fd127a561d517
John Watterson <john@breakthrewbrew.com>
2020-05-07 18:18:20 -0600

    :crystal_ball: Turn search component into class component that has a state of the search term entered by user

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react';
 import './SearchForm.css';
 
 class SearchForm extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  };
+
+  updateSearchValueOnChange = (event) => {
+    event.preventDefault();
+    this.setState({value: event.target.value});
+  };
+
+  sendUpSearchValueOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.filterNewsArticles(this.state.value);
+  };
 
   render() {
     return(
       <section className="search-section">
-        <input placeholder="Search All Article Here"></input>
-        <button>Search</button>
+        <form>
+          <input placeholder="Search Topic For Articles!" onChange={this.updateSearchValueOnChange}></input>
+          <button type="submit" onClick={this.sendUpSearchValueOnSubmit}>Search</button>
+        </form>
       </section>
     );
-  }
+  };
 }
 
 export default SearchForm;
