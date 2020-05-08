19296ba531c8d85f24000e42ee600773e31c4024
John Watterson <john@breakthrewbrew.com>
2020-05-07 18:13:25 -0600

    :crystal_ball: Reconfigure <a> so that entire box at bottom of news article is clickable to full article

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -5,9 +5,12 @@ const NewsArticle = (props) => {
   return(
     <article className="news-article">
       <h3>{props.headline}</h3>
+      <hr></hr>
       <p>{props.description}</p>
       <img src={props.img} alt=""/>
-      <a href={props.url}>Click Here to See Full Article</a>
+      <a href={props.url} rel="noopener">
+          <p id="hyperlink">Click to see full article <span className="arrow-icon">➜</span></p>
+      </a>
     </article>
   );
 }
